112464cfe0d86d2725d77313015ebad9a16a6ef7
Colk-tech <master@colk.dev>
2024-06-20 14:48:24 +0900

    fix: fix mypy problems in `run_command.py`

--- run_command.py
+++ run_command.py
@@ -1,16 +1,19 @@
+import select
 import subprocess
 import sys
+from typing import List
 
 from dotenv import load_dotenv
 
-# List of dotenv files to load, in order of priority (last one has the highest priority)
-DOTENV_FILES = [
+# List of dotenv files to load, in order of priority
+# (last one has the highest priority)
+DOTENV_FILES: List[str] = [
     "./.example.env",
     "./.local.env",
 ]
 
 
-def load_env_vars():
+def load_env_vars() -> None:
     for dotenv_file in DOTENV_FILES:
         try:
             print(f"Loading env vars from: {dotenv_file}")
@@ -20,19 +23,46 @@ def load_env_vars():
             continue
 
 
-def run_command(cmd: str):
-    process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+def run_command(cmd: str) -> int:
+    process = subprocess.Popen(
+        cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+    )
+
+    if process.stdout is None or process.stderr is None:
+        raise ValueError("Failed to capture stdout or stderr")
+
+    stdout, stderr = process.stdout, process.stderr
 
     while True:
-        output = process.stdout.readline()
-        error = process.stderr.readline()
+        reads = [stdout.fileno(), stderr.fileno()]
+        readable, _, _ = select.select(reads, [], [])
 
+        for r in readable:
+            if r == stdout.fileno():
+                output = stdout.readline()
+                if output:
+                    print(output.decode("utf-8").strip())
+            if r == stderr.fileno():
+                error = stderr.readline()
+                if error:
+                    print(error.decode("utf-8").strip(), file=sys.stderr)
+
+        if process.poll() is not None:
+            break
+
+    # Ensure all remaining output is printed
+    while True:
+        output = stdout.readline()
         if output:
             print(output.decode("utf-8").strip())
+        else:
+            break
+
+    while True:
+        error = stderr.readline()
         if error:
             print(error.decode("utf-8").strip(), file=sys.stderr)
-
-        if not output and not error and process.poll() is not None:
+        else:
             break
 
     return process.returncode
@@ -48,3 +78,4 @@ if __name__ == "__main__":
 
     exit_code = run_command(command)
     print(f"Command exited with code: {exit_code}")
+    sys.exit(exit_code)
